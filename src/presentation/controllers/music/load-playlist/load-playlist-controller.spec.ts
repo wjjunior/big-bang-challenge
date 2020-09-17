4f6a1a5fe237dc3e311cf46602b2110f8770c987
Wagner <wagner.junior30@gmail.com>
2020-09-17 17:41:48 -0300

    refactor: ensure LoadPlaylistController works with more than one object

--- src/presentation/controllers/music/load-playlist/load-playlist-controller.spec.ts
+++ src/presentation/controllers/music/load-playlist/load-playlist-controller.spec.ts
@@ -3,9 +3,14 @@ import { MusicModel, LoadPlaylist } from './load-playlist-controller-protocols'
 import { ok, serverError } from '../../../helpers/http/http-helper'
 
 const makeFakePlaylist = (): MusicModel[] => {
-  return [{
-    name: 'any_name'
-  }]
+  return [
+    {
+      name: 'any_name'
+    },
+    {
+      name: 'other_name'
+    }
+  ]
 }
 
 interface SutTypes {
@@ -16,7 +21,7 @@ interface SutTypes {
 const makeLoadPlaylist = (): LoadPlaylist => {
   class LoadPlaylistStub implements LoadPlaylist {
     async load (): Promise<MusicModel[]> {
-      return new Promise(resolve => resolve(makeFakePlaylist()))
+      return new Promise((resolve) => resolve(makeFakePlaylist()))
     }
   }
   return new LoadPlaylistStub()
@@ -47,7 +52,11 @@ describe('LoadPlaylist Controller', () => {
 
   test('Should return 500 if loadPlanets throws', async () => {
     const { sut, loadPlaylistStub } = makeSut()
-    jest.spyOn(loadPlaylistStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest
+      .spyOn(loadPlaylistStub, 'load')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(serverError(new Error()))
   })
