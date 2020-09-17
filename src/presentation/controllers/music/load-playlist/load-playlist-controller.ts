a5ad00d1842b4c545971d01ff0b8ba2a7923c468
Wagner <wagner.junior30@gmail.com>
2020-09-17 17:40:00 -0300

    feat: ensure LoadPlaylistController returns 500 if LoadPlaylistUseCase throws

--- src/presentation/controllers/music/load-playlist/load-playlist-controller.ts
+++ src/presentation/controllers/music/load-playlist/load-playlist-controller.ts
@@ -1,10 +1,15 @@
 import { Controller, HttpRequest, HttpResponse, LoadPlaylist } from './load-playlist-controller-protocols'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 
 export class LoadPlaylistController implements Controller {
   constructor (private readonly loadPlaylist: LoadPlaylist) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const playlist = await this.loadPlaylist.load()
-    return ok(playlist)
+    try {
+      const playlist = await this.loadPlaylist.load()
+      return ok(playlist)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
