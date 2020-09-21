1abdc8bb4784a84dd4e6da28d26fcac2b13d89c2
Wagner <wagner.junior30@gmail.com>
2020-09-21 20:30:29 -0300

    test: ensure WeatherRepository load a temperature on success

---
+++ src/infra/db/open-weather/weather-repository.spec.ts
@@ -0,0 +1,57 @@
+import { WeatherRepository } from './weather-repository'
+import OpenWeatherApi from './helpers/open-weather-api'
+
+beforeAll(() => {
+  jest
+    .spyOn(OpenWeatherApi.prototype, 'getTemperatureByCityName')
+    .mockReturnValueOnce(
+      new Promise((resolve) =>
+        resolve({
+          main: {
+            temp: 200
+          }
+        })
+      )
+    )
+
+  jest.spyOn(OpenWeatherApi.prototype, 'getTemperatureByCoordinates').mockReturnValueOnce(
+    new Promise((resolve) =>
+      resolve({
+        main: {
+          temp: 150
+        }
+      })
+    )
+  )
+})
+
+interface SutTypes {
+  sut: WeatherRepository
+}
+const makeSut = (): SutTypes => {
+  const sut = new WeatherRepository()
+  return {
+    sut
+  }
+}
+
+describe('Weather Repository', () => {
+  describe('loadTemperature()', () => {
+    test('Should load the temperature by city name', async () => {
+      const { sut } = makeSut()
+      const temp = await sut.loadTemperature({
+        cityName: 'any_city'
+      })
+      expect(temp).toEqual(200)
+    })
+
+    test('Should load the temperature by coordinates', async () => {
+      const { sut } = makeSut()
+      const temp = await sut.loadTemperature({
+        lat: 123,
+        lon: 321
+      })
+      expect(temp).toEqual(150)
+    })
+  })
+})
